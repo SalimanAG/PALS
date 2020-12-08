2e0d4101a0f561c8a4dc4bd06f691213441dc8e3
DJOSSOU Peter Ive <ivedjossou@gmail.com>
2020-12-08 16:35:08 +0100

    Initialisation

---
+++ src/main/java/com/sil/gpc/GuichetuniqApplication.java
@@ -0,0 +1,13 @@
+package com.sil.gpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuichetuniqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuichetuniqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sil/gpc/GuichetuniqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sil.gpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuichetuniqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
