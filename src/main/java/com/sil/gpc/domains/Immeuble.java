cb246f2cde4ab50a645a6f349308399892541dae
DJOSSOU Peter Ive <ivedjossou@gmail.com>
2021-01-28 11:22:51 +0100

    Valeur locative

--- src/main/java/com/sil/gpc/domains/Immeuble.java
+++ src/main/java/com/sil/gpc/domains/Immeuble.java
@@ -19,9 +19,18 @@ public class Immeuble implements Serializable{
 	private String libIm;
 	private String localisationIm;
 	private boolean etatIm;
-	//Liaison avec QuartierRepository
+	private double superficie;
+	private double valUnit;
+	private String stuctResp;
+	private String autre;
+	//Liaison avec Quartier
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER,targetEntity = Arrondissement.class)
+	@JoinColumn(name="codeArrondi", referencedColumnName = "codeArrondi", nullable = true)
+	private Arrondissement arrondissement;
+
+	//Liaison avec Arrondissement
 	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER,targetEntity = Quartier.class)
-	@JoinColumn(name="codeQuartier", referencedColumnName = "codeQuartier", nullable = false)
+	@JoinColumn(name="codeQuartier", referencedColumnName = "codeQuartier", nullable = true)
 	private Quartier quartier;
 	
 	//Liaison  avec TypeImmeuble
@@ -33,36 +42,163 @@ public class Immeuble implements Serializable{
 			@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER,targetEntity = SiteMarcher.class)
 			@JoinColumn(name="codeSite", referencedColumnName = "codeSite", nullable = false)
 			private SiteMarcher siteMarcher;
-		
-	public Immeuble() {
-		super();
-	}
+	
+	/**
+			 * @param codeIm
+			 * @param libIm
+			 * @param localisationIm
+			 * @param etatIm
+			 * @param superficie
+			 * @param valUnit
+			 * @param stuctResp
+			 * @param autre
+			 * @param arrondissement
+			 * @param quartier
+			 * @param typeImmeuble
+			 * @param siteMarcher
+			 */
+			public Immeuble(String codeIm, String libIm, String localisationIm, boolean etatIm, double superficie,
+					double valUnit, String stuctResp, String autre, Arrondissement arrondissement, Quartier quartier,
+					TypeImmeuble typeImmeuble, SiteMarcher siteMarcher) {
+				this.codeIm = codeIm;
+				this.libIm = libIm;
+				this.localisationIm = localisationIm;
+				this.etatIm = etatIm;
+				this.superficie = superficie;
+				this.valUnit = valUnit;
+				this.stuctResp = stuctResp;
+				this.autre = autre;
+				this.arrondissement = arrondissement;
+				this.quartier = quartier;
+				this.typeImmeuble = typeImmeuble;
+				this.siteMarcher = siteMarcher;
+			}
 
-
+	/**
+	 * @return the codeIm
+	 */
 	public String getCodeIm() {
 		return codeIm;
 	}
 
+	/**
+	 * @param codeIm the codeIm to set
+	 */
 	public void setCodeIm(String codeIm) {
 		this.codeIm = codeIm;
 	}
 
+	/**
+	 * @return the libIm
+	 */
+	public String getLibIm() {
+		return libIm;
+	}
+
+	/**
+	 * @param libIm the libIm to set
+	 */
+	public void setLibIm(String libIm) {
+		this.libIm = libIm;
+	}
+
+	/**
+	 * @return the localisationIm
+	 */
 	public String getLocalisationIm() {
 		return localisationIm;
 	}
 
+	/**
+	 * @param localisationIm the localisationIm to set
+	 */
 	public void setLocalisationIm(String localisationIm) {
 		this.localisationIm = localisationIm;
 	}
 
+	/**
+	 * @return the etatIm
+	 */
 	public boolean isEtatIm() {
 		return etatIm;
 	}
 
+	/**
+	 * @param etatIm the etatIm to set
+	 */
 	public void setEtatIm(boolean etatIm) {
 		this.etatIm = etatIm;
 	}
 
+	/**
+	 * @return the superficie
+	 */
+	public double getSuperficie() {
+		return superficie;
+	}
+
+	/**
+	 * @param superficie the superficie to set
+	 */
+	public void setSuperficie(double superficie) {
+		this.superficie = superficie;
+	}
+
+	/**
+	 * @return the valUnit
+	 */
+	public double getValUnit() {
+		return valUnit;
+	}
+
+	/**
+	 * @param valUnit the valUnit to set
+	 */
+	public void setValUnit(double valUnit) {
+		this.valUnit = valUnit;
+	}
+
+	/**
+	 * @return the stuctResp
+	 */
+	public String getStuctResp() {
+		return stuctResp;
+	}
+
+	/**
+	 * @param stuctResp the stuctResp to set
+	 */
+	public void setStuctResp(String stuctResp) {
+		this.stuctResp = stuctResp;
+	}
+
+	/**
+	 * @return the autre
+	 */
+	public String getAutre() {
+		return autre;
+	}
+
+	/**
+	 * @param autre the autre to set
+	 */
+	public void setAutre(String autre) {
+		this.autre = autre;
+	}
+
+	/**
+	 * @return the arrondissement
+	 */
+	public Arrondissement getArrondissement() {
+		return arrondissement;
+	}
+
+	/**
+	 * @param arrondissement the arrondissement to set
+	 */
+	public void setArrondissement(Arrondissement arrondissement) {
+		this.arrondissement = arrondissement;
+	}
 
 	/**
 	 * @return the quartier
@@ -71,14 +207,12 @@ public class Immeuble implements Serializable{
 		return quartier;
 	}
 
-
 	/**
 	 * @param quartier the quartier to set
 	 */
 	public void setQuartier(Quartier quartier) {
 		this.quartier = quartier;
 	}
-
 
 	/**
 	 * @return the typeImmeuble
@@ -87,7 +221,6 @@ public class Immeuble implements Serializable{
 		return typeImmeuble;
 	}
 
-
 	/**
 	 * @param typeImmeuble the typeImmeuble to set
 	 */
@@ -95,37 +228,18 @@ public class Immeuble implements Serializable{
 		this.typeImmeuble = typeImmeuble;
 	}
 
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(codeIm, etatIm, localisationIm, quartier, typeImmeuble);
+	/**
+	 * @return the siteMarcher
+	 */
+	public SiteMarcher getSiteMarcher() {
+		return siteMarcher;
 	}
 
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		Immeuble other = (Immeuble) obj;
-		return Objects.equals(codeIm, other.codeIm) && etatIm == other.etatIm
-				&& Objects.equals(localisationIm, other.localisationIm) && Objects.equals(quartier, other.quartier)
-				&& Objects.equals(typeImmeuble, other.typeImmeuble);
+	/**
+	 * @param siteMarcher the siteMarcher to set
+	 */
+	public void setSiteMarcher(SiteMarcher siteMarcher) {
+		this.siteMarcher = siteMarcher;
 	}
 
-
-	public String getLibIm() {
-		return libIm;
-	}
-
-
-	public void setLibIm(String libIm) {
-		this.libIm = libIm;
-	}	
 }
