5426a41360789c585d457411ff4fea0c53c2e277
DJOSSOU Peter Ive <ivedjossou@gmail.com>
2021-04-23 16:07:26 +0200

    Inventaire revu

--- src/main/java/com/sil/gpc/repositories/InventaireRepository.java
+++ src/main/java/com/sil/gpc/repositories/InventaireRepository.java
@@ -1,8 +1,5 @@
 package com.sil.gpc.repositories;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,31 +8,9 @@ import com.sil.gpc.domains.Inventaire;
 
 @Repository
 public interface InventaireRepository extends JpaRepository<Inventaire, String>{
-	@Override
-	default void deleteById(String id) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	@Override
-	default List<Inventaire> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	default Optional<Inventaire> findById(String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	default <S extends Inventaire> S save(S entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
+
 	@Query(value="SELECT valeur FROM Inventaire WHERE code_exercice = ?1 ORDER BY valeur DESC LIMIT 1;"
 			, nativeQuery = true)
 	Integer findLastNumUsed(String codeExo);
+	
 }
--- src/main/java/com/sil/gpc/repositories/LigneInventaireRepository.java
+++ src/main/java/com/sil/gpc/repositories/LigneInventaireRepository.java
@@ -1,7 +1,5 @@
 package com.sil.gpc.repositories;
 
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.sil.gpc.domains.LigneInventaire;
@@ -9,28 +7,5 @@ import com.sil.gpc.domains.LigneInventaire;
 @Repository
 public interface LigneInventaireRepository extends JpaRepository<LigneInventaire, Long>{
 	
-	@Override
-	default void deleteById(Long id) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	@Override
-	default List<LigneInventaire> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	default Optional<LigneInventaire> findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	default <S extends LigneInventaire> S save(S entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
