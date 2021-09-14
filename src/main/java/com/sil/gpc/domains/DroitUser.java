b88f51d6fe05531d87861d4784472c7db419b597
Saliman <salimanagonhealberic@gmail.com>
2021-09-14 13:08:08 +0100

    m

--- src/main/java/com/sil/gpc/domains/DroitUser.java
+++ src/main/java/com/sil/gpc/domains/DroitUser.java
@@ -1,5 +1,6 @@
 package com.sil.gpc.domains;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +12,7 @@ public class DroitUser {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long idDroitUser;
+	@Column(unique = true)
 	private String codeDroitUser;
 	private String libDroitUser;
 	private String descriDroitUser;
