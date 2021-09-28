d2e3087bbad59626ecc0552de3a98a23179a4586
Saliman <salimanagonhealberic@gmail.com>
2021-09-28 18:21:29 +0100

    m

--- src/main/java/com/sil/gpc/domains/AffectDroitGroupUser.java
+++ src/main/java/com/sil/gpc/domains/AffectDroitGroupUser.java
@@ -14,7 +14,7 @@ import javax.persistence.ManyToOne;
 public class AffectDroitGroupUser {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long idAffectDroitGroup;
 	
 	//Liaison  avec Droits des Utilisateurs
